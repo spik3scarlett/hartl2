60602304a1a66214ee13ed19cdf0e0cf8278b1dc
Spik3Scarlett <gitarsitar@gmail.com>
2018-05-03 22:20:08 +0200

    walka z heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
    get '/nowy-uczen', to: 'users#new'
    post '/nowy-uczen', to: 'users#create'
 
-   root 'podaj_slowko#losuj_obrazek'
+   root 'sessions#new' # 'podaj_slowko#losuj_obrazek'
    post "/", to: 'podaj_slowko#check'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    get '/what-can-you-see', to: 'podaj_slowko#losuj_obrazek'
