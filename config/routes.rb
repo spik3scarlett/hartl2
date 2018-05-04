113829df0881e4f72ecd6291271c045ec6d17bd2
Spik3Scarlett <gitarsitar@gmail.com>
2018-05-04 21:11:44 +0200

    Finish chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
    resources :users
    resources :account_activations, only: [:edit]
+   resources :password_resets,     only: [:new, :create, :edit, :update]
+
    get '/nowy-uczen', to: 'users#new'
    post '/nowy-uczen', to: 'users#create'
 
